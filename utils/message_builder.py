f106d9ad78297430278df788d868f581879775df
GJ98 <gangjeong22@gmail.com>
2024-11-15 06:57:00 +0000

    feat: OpenAI api 모듈 구현 프롬프트 작성 클래스인 message builder 구현 #4

---
+++ utils/message_builder.py
@@ -0,0 +1,86 @@
+import pandas as pd
+from ast import literal_eval
+
+
+class MessageBuilder:
+
+    def __init__(
+        self,
+    ):
+        self.PROMPT_NO_QUESTION_PLUS = """지문:
+                {paragraph}
+
+                질문:
+                {question}
+
+                선택지:
+                {choices}
+
+                1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.
+                정답: {answer}"""
+
+        self.PROMPT_QUESTION_PLUS = """지문:
+                {paragraph}
+
+                질문:
+                {question}
+
+                <보기>:
+                {question_plus}
+
+                선택지:
+                {choices}
+
+                1, 2, 3, 4, 5 중에 하나를 정답으로 고르세요.
+                정답: {answer}"""
+
+    def create_user_message(self, row):
+        problem = literal_eval(row["problems"])
+        choices_string = "\n".join(
+            [f"{idx + 1} - {choice}" for idx, choice in enumerate(problem["choices"])]
+        )
+
+        if not pd.isna(row["question_plus"]):
+            user_message = self.PROMPT_QUESTION_PLUS.format(
+                paragraph=row["paragraph"],
+                question=problem["question"],
+                question_plus=row["question_plus"],
+                choices=choices_string,
+                answer=problem["answer"],
+            )
+        else:
+            user_message = self.PROMPT_NO_QUESTION_PLUS.format(
+                paragraph=row["paragraph"],
+                question=problem["question"],
+                choices=choices_string,
+                answer=problem["answer"],
+            )
+
+        return user_message
+
+    def create_message_list(self, system_message, file_path):
+
+        df = pd.read_csv(file_path)
+
+        id_list, message_list = [], []
+
+        for index, row in df.iterrows():
+            id_list.append(row["id"])
+            user_message = self.create_user_message(row)
+
+            message = [
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": user_message},
+            ]
+            message_list.append(message)
+
+        return id_list, message_list
+
+
+if __name__ == "__main__":
+    message_builder = MessageBuilder()
+    id_list, message_list = message_builder.create_message_list(
+        system_message="아래 문제 정답이 지문 안에 있는지에 대한 여부 알려주세요.",
+        file_path="/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/data/train.csv",
+    )
+    print(message_list[0])
