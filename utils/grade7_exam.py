a54fc794a532d6bb17195d383ca13f34003a9100
GJ98 <gangjeong22@gmail.com>
2024-11-17 11:35:58 +0000

    refactor: 7급 공채 데이터 추출 클래스 리펙토링
    #13

--- utils/grade7_exam.py
+++ utils/grade7_exam.py
@@ -1,150 +1,127 @@
 import pdfplumber
 import re
 import json
-from PyPDF2 import PdfReader
 import unicodedata
+from collections import deque
 
 
 class ExtractGrade7Exam:
 
     def __init__(self, file_path):
-
         self.file_path = file_path
 
-    def is_problem_start_point(self, text):
-        # print(text)
-        match = re.search(r"^문 (\d{2}|\d{1})\.", text)
-        # match = re.search(r"^(\d{2}|\d{1})\.", text)
+    def run(self):
+        question_list = list()
 
-        if match:
-            return (
-                match.group().replace("문", "problem").replace(".", ""),
-                text[match.end() :],
-            )
-        else:
-            return None
-
-    def run(
-        self,
-    ):
-        result = list()
-
+        # 문제 추출
         with pdfplumber.open(self.file_path) as pdf:
             for page in pdf.pages:
                 width, height = page.width, page.height
-                left_layout = (0, 0, width / 2, height)  # 상하로 범위 확장
+                left_layout = (0, 0, width / 2, height)
                 right_layout = (width / 2, 0, width, height)
 
-                left_text = page.within_bbox(left_layout).extract_text()
-                right_text = page.within_bbox(right_layout).extract_text()
+                left_lines = page.within_bbox(left_layout).extract_text_lines()
+                right_lines = page.within_bbox(right_layout).extract_text_lines()
 
-                sub_result1 = self.extract_page(left_text)
-                sub_result2 = self.extract_page(right_text)
-                result.extend(sub_result1)
-                result.extend(sub_result2)
+                left_images = page.within_bbox(left_layout).images
+                right_images = page.within_bbox(right_layout).images
 
-        with open(
-            f"{self.file_path.split('/')[-1].split('.')[0]}.json", "w", encoding="utf-8"
-        ) as json_file:
-            json.dump(result, json_file, ensure_ascii=False, indent=4)
+                left_q_list = self.extract_question(deque(left_lines), left_images)
+                right_q_list = self.extract_question(deque(right_lines), right_images)
+                question_list.extend(left_q_list)
+                question_list.extend(right_q_list)
 
-    def extract_page(self, page_text):
-        page_text = page_text.split("\n")
-        result_list = []
+        # 문제 파일로 저장
+        self.save_question(question_list)
 
-        index = 0
-        # 문제 시작 위치 찾기
-        while index < len(page_text):
-            is_start_point = self.is_problem_start_point(page_text[index])
+    def extract_question(self, page, images):
+        question_list = []
 
-            if is_start_point is not None:
+        # 해당 페이지의 첫 문제 시작 줄 찾기
+        while page:
+            line = page[0]
+            if re.match(r"^문 (\d{2}|\d{1})\.", line["text"]):
                 break
-            index += 1
+            page.popleft()
 
-        while index < len(page_text):
-            question_id, q_start = self.is_problem_start_point(page_text[index])
+        # 문제 추출
+        while page:
+            line = page.popleft()
+            match = re.match(r"^문 (\d{2}|\d{1})\.", line["text"])
+            question_id = re.sub(f"[^0-9]", "", line["text"][: match.end()])
 
-            # 질문 찾기
-            question = [q_start]
-            while index < len(page_text):
-                q_end_pattern1 = re.search(r"\? \(.*\)$", page_text[index])
-                q_end_pattern4 = re.search(r"\?$", page_text[index])
-                q_end_pattern2 = re.search(r"\? \(.*$", page_text[index])
-
-                index += 1
-                if q_end_pattern1 is not None:
+            # 1. 질문 추출
+            question = [line]
+            while page:
+                # 1. 질문 ? (추가 설명) -> 질문 끝
+                if re.search(r"\? \(.*\)$", line["text"]):
                     break
 
-                if q_end_pattern4 is not None:
-                    if re.search(r"(^\(가\)|^\( ㉠ \))", page_text[index]):
-                        break
-                    if re.search(r"^\(.*", page_text[index]) is None:
-                        break
-
-                    question.append(page_text[index])
-                    while not re.search(r"\)$", page_text[index]):
-                        index += 1
-                        question.append(page_text[index])
-                    index += 1
+                # 2. 질문 ? -> 다음 줄에 추가 설명 올 수 있음
+                if re.search(r"\?$", line["text"]):
+                    while page:
+                        if re.search(r"(^\(가\)|^\( ㉠ \))", page[0]["text"]):
+                            break
+                        if not re.search(r"^\(.*", page[0]["text"]):
+                            break
+                        question.append(page.popleft())
                     break
 
-                if q_end_pattern2 is not None:
-                    while index < len(page_text):
-                        question.append(page_text[index])
-
-                        q_end_pattern3 = re.search(r"\)$", page_text[index])
-                        index += 1
-                        if q_end_pattern3 is not None:
+                # 3. 질문 ? (추가 설명 -> 다음 줄에 추가 설명이 이어짐
+                if re.search(r"\? \(.*$", line["text"]):
+                    while page:
+                        line = page.popleft()
+                        question.append(line)
+                        if re.search(r"\)$", line["text"]):
                             break
                     break
-                question.append(page_text[index])
 
-            # 보기 찾기
+                line = page.popleft()
+                question.append(line)
+
+            # 보기 추출
             context = []
-            while index < len(page_text):
-                if re.search("①", page_text[index]) is not None:
+            while page:
+                if re.search("①", page[0]["text"]):
                     break
-                else:
-                    context.append(page_text[index])
-                    index += 1
-                    continue
+                context.append(page.popleft())
 
             # 선지 찾기
-            print(page_text[index])
             choices = []
-            while index < len(page_text):
-                next_problem_start_point = self.is_problem_start_point(page_text[index])
-
-                if next_problem_start_point is not None:
+            while page:
+                if re.match(r"^문 (\d{2}|\d{1})\.", page[0]["text"]):
                     break
+                choices.append(page.popleft())
 
-                choices.append(page_text[index])
-                print(page_text[index])
-                index += 1
-
-            question = " ".join(question).strip()
-            context = " ".join(context).strip() if len(context) != 0 else None
-            choices = [
-                choice.strip()
-                for choice in re.split(r"(①|②|③|④)", " ".join(choices))[::2][1:]
+            # 문제에 그림 있을 경우 배제
+            if self.is_with_img(question, choices, images):
+                continue
+            # 추출한 데이터(지문, 보기, 선지) 저장
+            questionset = dict()
+            questionset["question_id"] = question_id
+            questionset["question"] = " ".join([q["text"] for q in question])
+            questionset["context"] = " ".join([c["text"] for c in context])
+            questionset["choices"] = [
+                re.sub("(①|②|③|④|⑤)", "", choice["text"]) for choice in choices
             ]
+            question_list.append(questionset)
 
-            print(question_id)
-            print("질문")
-            print(question)
-            print("보기")
-            print(context)
-            print("선지")
-            print(choices)
-            print("=" * 50)
-            result = dict()
-            result["question_id"] = question_id
-            result["question"] = question
-            result["context"] = context
-            result["choices"] = choices
-            result_list.append(result)
+        return question_list
 
-        return result_list
+    def save_question(self, question_list):
+        save_file_name = f"{self.file_path.split('/')[-1].split('.')[0]}"
+
+        with open(f"{save_file_name}.json", "w", encoding="utf-8") as json_file:
+            json.dump(question_list, json_file, ensure_ascii=False, indent=4)
+
+    def is_with_img(self, question, choices, images):
+        top = question[0]["top"]
+        bottom = choices[-1]["bottom"]
+
+        for img in images:
+            if top < img["top"] < bottom:
+                return True
+        return False
 
 
 # 질문과 정답 매핑 함수
@@ -183,8 +160,8 @@ def map_question_answer(q_file, a_file):
 
 if __name__ == "__main__":
     file_path_list = [
-        "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2020_심리학.pdf",
-        # "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2021_심리학.pdf",
+        # "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2020_심리학.pdf",
+        "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2021_심리학.pdf",
         # "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2022_심리학.pdf",
         # "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2023_심리학.pdf",
         # "/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/psychology/7급공채_2024_심리학.pdf",
