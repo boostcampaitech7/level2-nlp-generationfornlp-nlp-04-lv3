efc0e83a127d671e0097663bef3f69d5fa630d6b
CLM-BONNY <alstjs4739@kookmin.ac.kr>
2024-11-20 04:45:14 +0000

    feat: 기존 message_builder.py에Claude 메시지 형식 추가
    #26

--- utils/message_builder.py
+++ utils/message_builder.py
@@ -4,9 +4,9 @@ from ast import literal_eval
 
 class MessageBuilder:
 
-    def __init__(
-        self,
-    ):
+    def __init__(self, api_type="openai"):
+        self.api_type = api_type
+
         self.PROMPT_NO_QUESTION_PLUS = """지문:
                 {paragraph}
 
@@ -58,7 +58,7 @@ class MessageBuilder:
 
         return user_message
 
-    def create_message_list(self, system_message, file_path):
+    def create_message_list(self, file_path, system_message=""):
 
         df = pd.read_csv(file_path)
 
@@ -68,19 +68,28 @@ class MessageBuilder:
             id_list.append(row["id"])
             user_message = self.create_user_message(row)
 
-            message = [
-                {"role": "system", "content": system_message},
-                {"role": "user", "content": user_message},
-            ]
+            # OpenAI API 요청 메시지
+            if self.api_type == "openai":
+                message = [
+                    {"role": "system", "content": system_message},
+                    {"role": "user", "content": user_message},
+                ]
+            # Claude API 요청 메시지
+            elif self.api_type == "claude":
+                message = [{"role": "user", "content": user_message}]
+            # 다른 API 요청시 에러 발생
+            else:
+                raise ValueError(f"Unsupported API type: {self.api_type}")
+
             message_list.append(message)
 
         return id_list, message_list
 
 
 if __name__ == "__main__":
-    message_builder = MessageBuilder()
+    message_builder = MessageBuilder("claude")
     id_list, message_list = message_builder.create_message_list(
         system_message="아래 문제 정답이 지문 안에 있는지에 대한 여부 알려주세요.",
-        file_path="/data/ephemeral/home/gj/level2-nlp-generationfornlp-nlp-04-lv3/data/train.csv",
+        file_path="/data/ephemeral/home/ms/level2-nlp-generationfornlp-nlp-04-lv3/data/default/train.csv",
     )
     print(message_list[0])
