1fd846c99a1a7aebd2947cf72aa99ee774c8b1db
gityeop <freiheit517@icloud.com>
2024-11-21 04:33:37 +0000

    feat: job queue 루트 디렉토리 설정 추가

--- utils/worker.sh
+++ utils/worker.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-QUEUE_FILE="/data/ephemeral/home/level2-nlp-datacentric-nlp-11/job_queue.txt"
-LOG_FILE="/data/ephemeral/home/level2-nlp-datacentric-nlp-11/job_worker.log"
+
+# .env 파일 경로
+ENV_FILE="/data/ephemeral/home/level2-nlp-generationfornlp-nlp-04-lv3/.env"
+
+# .env 파일 로드
+if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
+else
+    echo ".env 파일을 찾을 수 없습니다: $ENV_FILE"
+    exit 1
+fi
+
+# ROOT_DIR 기반으로 경로 설정
+QUEUE_FILE="$ROOT_DIR/job_queue.txt"
+LOG_FILE="$ROOT_DIR/job_worker.log"
+
 source ~/.bashrc
+
 while true; do
     if [ -s "$QUEUE_FILE" ]; then
         # 첫 번째 줄 읽기
@@ -9,7 +24,13 @@ while true; do
 
         # 현재 시간 기록
         TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
-
+        # 유효한 명령어인지 확인
+        if [[ ! "$JOB" =~ ^[a-zA-Z0-9_./-]+ ]]; then
+            echo "[$TIMESTAMP] 잘못된 명령어: $JOB" >> "$LOG_FILE"
+            # 잘못된 명령어 제거
+            sed -i '1d' "$QUEUE_FILE"
+            continue
+        fi
         # 명령어 실행 및 로그 기록
         echo "[$TIMESTAMP] 실행 중: $JOB" >> "$LOG_FILE"
 
