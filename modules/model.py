5b1d77c3f25282a58680d60e19aa394ac83abd39
jhyeop <pizzauniv99@gmail.com>
2024-11-27 17:07:48 +0900

    feat: DPO 구현 및 모델, 트레이너에 Unsloth 적용
    #49

--- modules/model.py
+++ modules/model.py
@@ -1,56 +1,98 @@
 import torch
 from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from unsloth import FastLanguageModel
 
 
 class KsatModel:
-    def __init__(self, model_name_or_checkpoint_path, use_checkpoint=False):
+    def __init__(self, model_name_or_checkpoint_path, config, use_checkpoint=False):
         self.model_name_or_checkpoint_path = model_name_or_checkpoint_path
+        self.config = config
         self.use_checkpoint = use_checkpoint
         self.model = None
         self.tokenizer = None
         self.response_template = None
 
     def setup(self):
-        # setting tokenizer
-        self.tokenizer = AutoTokenizer.from_pretrained(
-            self.model_name_or_checkpoint_path,
-            trust_remote_code=True,
-        )
-        if not self.use_checkpoint:
-            special_tokens = self._get_special_tokens(
+        if not self.config.use_unsloth:
+            # setting tokenizer
+            self.tokenizer = AutoTokenizer.from_pretrained(
+                self.model_name_or_checkpoint_path,
+                trust_remote_code=True,
+            )
+            if not self.use_checkpoint:
+                special_tokens = self._get_special_tokens(
+                    self.model_name_or_checkpoint_path
+                )
+                special_tokens = {"additional_special_tokens": special_tokens}
+                self.tokenizer.add_special_tokens(special_tokens)
+
+                if self.tokenizer.chat_template == None:
+                    self.tokenizer.chat_template = self._get_chat_template(
+                        self.model_name_or_checkpoint_path
+                    )
+
+                self.tokenizer.pad_token = self.tokenizer.eos_token
+                self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+                self.tokenizer.padding_side = "right"
+
+            if self.use_checkpoint:
+                self.model = AutoPeftModelForCausalLM.from_pretrained(
+                    self.model_name_or_checkpoint_path,
+                    trust_remote_code=True,
+                    device_map="auto",
+                )
+            else:
+                self.model = AutoModelForCausalLM.from_pretrained(
+                    self.model_name_or_checkpoint_path,
+                    torch_dtype=torch.float16,
+                    trust_remote_code=True,
+                )
+                self.model.resize_token_embeddings(len(self.tokenizer))
+
+            self.response_template = self._get_response_template(
                 self.model_name_or_checkpoint_path
             )
-            special_tokens = {"additional_special_tokens": special_tokens}
-            self.tokenizer.add_special_tokens(special_tokens)
+        else:
+            self.model, self.tokenizer = FastLanguageModel.from_pretrained(
+                model_name = self.model_name_or_checkpoint_path,
+                max_seq_length = self.config.training_params.max_seq_length,
+                dtype = torch.float16,
+                load_in_4bit = False,
+            )
 
-            if self.tokenizer.chat_template == None:
-                self.tokenizer.chat_template = self._get_chat_template(
+            if not self.use_checkpoint:
+                special_tokens = self._get_special_tokens(
                     self.model_name_or_checkpoint_path
                 )
 
-            self.tokenizer.pad_token = self.tokenizer.eos_token
-            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
-            self.tokenizer.padding_side = "right"
+                if self.tokenizer.chat_template == None:
+                    self.tokenizer.chat_template = self._get_chat_template(
+                        self.model_name_or_checkpoint_path
+                    )
 
-        if self.use_checkpoint:
-            self.model = AutoPeftModelForCausalLM.from_pretrained(
-                self.model_name_or_checkpoint_path,
-                trust_remote_code=True,
-                # torch_dtype=torch.bfloat16,
-                device_map="auto",
-            )
-        else:
-            self.model = AutoModelForCausalLM.from_pretrained(
-                self.model_name_or_checkpoint_path,
-                torch_dtype=torch.float16,
-                trust_remote_code=True,
-            )
-            self.model.resize_token_embeddings(len(self.tokenizer))
+                self.tokenizer.pad_token = self.tokenizer.eos_token
+                self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+                self.tokenizer.padding_side = "right"
 
-        self.response_template = self._get_response_template(
-            self.model_name_or_checkpoint_path
-        )
+            self.response_template = self._get_response_template(
+                self.model_name_or_checkpoint_path
+            )
+
+            self.model = FastLanguageModel.get_peft_model(
+                self.model,
+                r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+                target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                                "gate_proj", "up_proj", "down_proj",],
+                lora_alpha = 16,
+                lora_dropout = 0, # Supports any, but = 0 is optimized
+                bias = "none",    # Supports any, but = "none" is optimized
+                # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+                use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+                random_state = 3407,
+                use_rslora = False,  # We support rank stabilized LoRA
+                loftq_config = None, # And LoftQ
+            )
 
     @staticmethod
     def _get_special_tokens(model_name):
@@ -76,7 +118,7 @@ class KsatModel:
             response_template = "<|start_header_id|>assistant<|end_header_id|>\n\n"
         elif model_name == "CarrotAI/Llama-3.2-Rabbit-Ko-3B-Instruct":
             response_template = "<|start_header_id|>assistant<|end_header_id|>\n\n"
-        elif model_name == "Qwen/Qwen2.5-3B-Instruct":
+        elif model_name in ["Qwen/Qwen2.5-3B-Instruct", "unsloth/Qwen2.5-7B-Instruct", "Qwen/Qwen2.5-7B-Instruct"]:
             response_template = "<|im_start|>assistant\n"
         else:
             response_template = ""
--- train.py
+++ train.py
@@ -1,31 +1,53 @@
 import os
 import hydra
 import wandb
+import torch
 from dotenv import load_dotenv
+from omegaconf import OmegaConf
 
 from utils.common import set_seed
 from modules.model import KsatModel
-from modules.trainer import KsatTrainer, KsatCoTTrainer
 from modules.data_module import KsatDataModule
+from modules.trainer import KsatTrainer, KsatCoTTrainer, KsatDPOTrainer
 
 
 @hydra.main(config_path="./config", config_name="config", version_base=None)
 def main(config):
     set_seed(42)
 
-    # 1. model, tokenizer 세팅
-    model_module = KsatModel(config.model_name)
-    model_module.setup()
+    if config.trainer_type in ["SFT", "CoT"]:
+        # 1. model, tokenizer 세팅
+        model_module = KsatModel(config.model_name, config)
+        model_module.setup()
 
-    # 2. data module 세팅
-    data_module = KsatDataModule(model_module.tokenizer, config.data)
-    data_module.setup("train")
+        # 2. data module 세팅
+        data_module = KsatDataModule(model_module.tokenizer, config)
+        data_module.setup("train")
 
-    # 3. trainer 세팅
-    if config.trainer_type == "SFT":
-        trainer_module = KsatTrainer(model_module, data_module, config)
-    elif config.trainer_type == "CoT":
-        trainer_module = KsatCoTTrainer(model_module, data_module, config)
+        # 3. trainer 세팅
+        if config.trainer_type == "SFT":
+            trainer_module = KsatTrainer(model_module, data_module, config)
+        elif config.trainer_type == "CoT":
+            trainer_module = KsatCoTTrainer(model_module, data_module, config)
+    
+    elif config.trainer_type == "DPO":
+        checkpoint_dir = "Qwen-Qwen2.5-3B-Instruct_CoT_data=default_lr=2e-05_bz=1_acc=0.7013"
+        ROOT_DIR = os.getenv("ROOT_DIR")
+        run_path = os.path.join(ROOT_DIR, f"checkpoints/{checkpoint_dir}")
+        sub_dir_list = os.listdir(run_path)
+        checkpoint_name = [
+            item for item in sub_dir_list if os.path.isdir(os.path.join(run_path, item))
+        ][0]
+        # /checkpoints/{run_name}/checkpoint-{step}
+        checkpoint_path = os.path.join(run_path, checkpoint_name)
+
+        model_module = KsatModel(checkpoint_path, config)
+        model_module.setup()
+
+        data_module = KsatDataModule(model_module.tokenizer, config)
+        data_module.setup("train")
+
+        trainer_module = KsatDPOTrainer(model_module, data_module, config)
 
     # 4. train
     trainer_module.train()
